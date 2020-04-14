27df60c39013fa641847ae4efed9f462a48a43c4
rcallow <rcallow87@gmail.com>
2020-04-14 13:15:14 +1000

    Jave event handling added, js event handling removed

--- src/rjc/Controller.java
+++ src/rjc/Controller.java
@@ -1,4 +1,22 @@
 package rjc;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    @FXML private TextArea textArea;
+
+    @FXML protected void handleNewButtonAction(ActionEvent event) {textArea.setText("");}
+    @FXML protected void handleOpenButtonAction(ActionEvent event) {}
+    @FXML protected void handleSaveButtonAction(ActionEvent event) {}
+    @FXML protected void handleSaveAsButtonAction(ActionEvent event) {}
+    @FXML protected void handlePageSetupButtonAction(ActionEvent event) {}
+    @FXML protected void handlePrintButtonAction(ActionEvent event) {}
+    @FXML protected void handleExitButtonAction(ActionEvent event) {}
+
+    @FXML protected void handleUndoButtonAction(ActionEvent event) {}
+    @FXML protected void handleCutButtonAction(ActionEvent event) {}
+    @FXML protected void handleCopyButtonAction(ActionEvent event) {}
+    @FXML protected void handlePasteButtonAction(ActionEvent event) {}
 }
